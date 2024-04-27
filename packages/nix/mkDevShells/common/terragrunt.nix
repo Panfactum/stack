035a77d8ba26a57509661e7acec0b01705adb8e4
fullykubed <github@fullstackjack.io>
2024-04-27 09:46:24 -0400

    feat: terragrunt performance improvements

--- packages/nix/mkDevShells/common/terragrunt.nix
+++ packages/nix/mkDevShells/common/terragrunt.nix
@@ -1,9 +1,26 @@
+# The wrapper serves the following purposes
+# - Prevents LFS files from being downloaded
+# - Sets up the provider cache when using run-all command (https://terragrunt.gruntwork.io/docs/features/provider-cache/)
 let
   system = builtins.currentSystem;
   nixpkgsSrc = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs";
-    rev = "080a4a27f206d07724b88da096e27ef63401a504";
+    rev = "92d295f588631b0db2da509f381b4fb1e74173c5";
     shallow = true;
   };
   pkgs = import nixpkgsSrc { inherit system; };
-in pkgs.terragrunt
+in pkgs.writeShellScriptBin "terragrunt" ''
+  #!/bin/env bash
+
+  export GIT_LFS_SKIP_SMUDGE=1
+  for arg in "$@"
+  do
+      if [[ "$arg" == "run-all" ]]; then
+          export TERRAGRUNT_PROVIDER_CACHE=1
+          export TERRAGRUNT_PROVIDER_CACHE_DIR="$TF_PLUGIN_CACHE_DIR"
+          ${pkgs.terragrunt}/bin/terragrunt "$@"
+          exit 0
+      fi
+  done
+  ${pkgs.terragrunt}/bin/terragrunt "$@"
+''
