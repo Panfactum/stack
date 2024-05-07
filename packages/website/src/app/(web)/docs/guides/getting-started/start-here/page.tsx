5061ec6ef0e52bac25da2f08aac7ffb8618972a9
fullykubed <github@fullstackjack.io>
2024-05-07 13:29:23 -0400

    feat: adds favicon meta tag to website

--- packages/website/src/app/(web)/docs/guides/getting-started/start-here/page.tsx
+++ packages/website/src/app/(web)/docs/guides/getting-started/start-here/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import Link from 'next/link'
 
 export const metadata: Metadata = {
-  title: 'Getting Started'
+  title: 'Start Here'
 }
 
 function Option (props: {href: string, text:string}) {
