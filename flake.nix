7bfd9d19e6d2c36eaaae5325f51b30e04cd2756a
fullykubed <github@fullstackjack.io>
2024-06-22 16:27:10 -0400

    wip: image build

--- flake.nix
+++ flake.nix
@@ -20,13 +20,20 @@
       };
     in {
       packages = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          isArm = pkgs.lib.hasPrefix "aarch64" system;
+          baseImage = pkgs.dockerTools.pullImage {
+            imageName = "gcr.io/distroless/base-debian12";
+            imageDigest = if isArm then "sha256:98088fae6231d8dc53541050bcd99d63f2bbeac7bc2878d8090f31c0ad98afab" else  "sha256:77618773587222150c59bcf9bffaf7a1e1caca9b1e00ff5b0d7f2614a51fb304";
+            sha256 =  if isArm then "sha256-I/Pfi4sIn7YdIU5KFc3QQg2SIDRXBNwEZ1g4PAiyBgM=" else "sha256-I/Pfi4sIn7YdIU5KFc3QQg2SIDRXBNwEZ1g4PAiyBgM=";
+          };
         in {
         # See https://github.com/NixOs/nixpkgs/pull/122608 for future optimizations
           image = pkgs.dockerTools.streamLayeredImage  {
             name = "panfactum";
             tag = "latest";
-            fromImage = "gcr.io/distroless/base-debian12:nonroot";
+            fromImage = baseImage;
 
             contents = pkgs.buildEnv {
               name = "image-root";
