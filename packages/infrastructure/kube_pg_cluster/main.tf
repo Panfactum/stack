f6aac7a3225820ea2abe6f4d328c2224e4eaf293
Wes Braga <wesbragagt@gmail.com>
2024-07-30 15:22:43 -0500

    feat: add shared_buffers parameter calculated from total memory

--- packages/infrastructure/kube_pg_cluster/main.tf
+++ packages/infrastructure/kube_pg_cluster/main.tf
@@ -353,6 +353,37 @@ resource "kubernetes_manifest" "postgres_cluster" {
         enablePodMonitor = var.monitoring_enabled
       }
 
+      postgresql = {
+        parameters = {
+          # These are default values that are set by the operator
+          archive_mode               = "on"
+          archive_timeout            = "5min"
+          dynamic_shared_memory_type = "posix"
+          log_destination            = "csvlog"
+          log_directory              = "/controller/log"
+          log_filename               = "postgres"
+          log_rotation_age           = "0"
+          log_rotation_size          = "0"
+          log_truncate_on_rotation   = "false"
+          logging_collector          = "on"
+          max_parallel_workers       = "32"
+          max_replication_slots      = "32"
+          max_worker_processes       = "32"
+          # The recommended value for shared_buffers is 25% of the total memory
+          shared_buffers             = "${var.pg_memory_mb / 4}MB"
+          shared_memory_type         = "mmap"
+          shared_preload_libraries   = ""
+          ssl_max_protocol_version   = "TLSv1.3"
+          ssl_min_protocol_version   = "TLSv1.3"
+          wal_keep_size              = "512MB"
+          wal_level                  = "logical"
+          wal_log_hints              = "on"
+          wal_receiver_timeout       = "5s"
+          wal_sender_timeout         = "5s"
+        }
+      }
+
+
       bootstrap = {
         initdb = {
           postInitSQL = [
