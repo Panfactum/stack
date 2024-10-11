0ce8427868634495f148aef3ef96f403519f488a
fullykubed <github@fullstackjack.io>
2024-10-11 18:14:37 -0400

    fix: stopped churn in aws_dns_records

--- packages/infrastructure/aws_dns_records/main.tf
+++ packages/infrastructure/aws_dns_records/main.tf
@@ -10,10 +10,44 @@ terraform {
 }
 
 locals {
-  cname_records = flatten([for zone, config in var.zones : [for record in config.cname_records : { zone : zone, record : record }]])
-  mx_records    = flatten([for zone, config in var.zones : [for record in config.mx_records : { zone : zone, record : record }]])
-  txt_records   = flatten([for zone, config in var.zones : [for record in config.txt_records : { zone : zone, record : record }]])
-  a_records     = flatten([for zone, config in var.zones : [for record in config.a_records : { zone : zone, record : record }]])
+  cname_records = { for config in flatten([for zone, config in var.zones : [for record in config.cname_records : {
+    type    = "CNAME"
+    zone_id = data.aws_route53_zone.zones[zone].zone_id,
+    name    = "${(endswith(record.subdomain, ".") || record.subdomain == "") ? record.subdomain : "${record.subdomain}."}${zone}",
+    records = [record.record]
+    ttl     = record.ttl
+  }]]) : "${config.zone_id}-CNAME-${config.name}" => config }
+
+  mx_records = { for config in flatten([for zone, config in var.zones : [for record in config.mx_records : {
+    type    = "MX"
+    zone_id = data.aws_route53_zone.zones[zone].zone_id,
+    name    = "${(endswith(record.subdomain, ".") || record.subdomain == "") ? record.subdomain : "${record.subdomain}."}${zone}",
+    records = record.records
+    ttl     = record.ttl
+  }]]) : "${config.zone_id}-MX-${config.name}" => config }
+
+  txt_records = { for config in flatten([for zone, config in var.zones : [for record in config.txt_records : {
+    type    = "TXT"
+    zone_id = data.aws_route53_zone.zones[zone].zone_id,
+    name    = "${(endswith(record.subdomain, ".") || record.subdomain == "") ? record.subdomain : "${record.subdomain}."}${zone}",
+    records = [for record in record.records : length(record) > 255 ? "${substr(record, 0, 255)}\"\"${substr(record, 255, length(record))}" : record] // Need to do a bit of cleaning based on record length due to AWS API limitations
+    ttl     = record.ttl
+  }]]) : "${config.zone_id}-TXT-${config.name}" => config }
+
+  a_records = { for config in flatten([for zone, config in var.zones : [for record in config.a_records : {
+    type    = "A"
+    zone_id = data.aws_route53_zone.zones[zone].zone_id,
+    name    = "${(endswith(record.subdomain, ".") || record.subdomain == "") ? record.subdomain : "${record.subdomain}."}${zone}",
+    records = record.records
+    ttl     = record.ttl
+  }]]) : "${config.zone_id}-A-${config.name}" => config }
+
+  records = merge(
+    local.cname_records,
+    local.mx_records,
+    local.txt_records,
+    local.a_records
+  )
 }
 
 ##########################################################################
@@ -25,43 +59,12 @@ data "aws_route53_zone" "zones" {
   name     = each.key
 }
 
-resource "aws_route53_record" "a" {
-  count           = length(local.a_records)
-  type            = "A"
-  zone_id         = data.aws_route53_zone.zones[local.a_records[count.index].zone].zone_id
-  name            = "${local.a_records[count.index].record.subdomain}${local.a_records[count.index].zone}"
-  records         = local.a_records[count.index].record.records
-  ttl             = local.a_records[count.index].record.ttl
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "mx" {
-  count           = length(local.mx_records)
-  type            = "MX"
-  zone_id         = data.aws_route53_zone.zones[local.mx_records[count.index].zone].zone_id
-  name            = "${local.mx_records[count.index].record.subdomain}${local.mx_records[count.index].zone}"
-  records         = local.mx_records[count.index].record.records
-  ttl             = local.mx_records[count.index].record.ttl
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "cname" {
-  count           = length(local.cname_records)
-  type            = "CNAME"
-  zone_id         = data.aws_route53_zone.zones[local.cname_records[count.index].zone].zone_id
-  name            = "${local.cname_records[count.index].record.subdomain}${local.cname_records[count.index].zone}"
-  records         = [local.cname_records[count.index].record.record]
-  ttl             = local.cname_records[count.index].record.ttl
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "txt" {
-  count   = length(local.txt_records)
-  type    = "TXT"
-  zone_id = data.aws_route53_zone.zones[local.txt_records[count.index].zone].zone_id
-  name    = "${local.txt_records[count.index].record.subdomain}${local.txt_records[count.index].zone}"
-  // Need to do a bit of cleaning based on record length due to AWS API limitations
-  records         = [for record in local.txt_records[count.index].record.records : length(record) > 255 ? "${substr(record, 0, 255)}\"\"${substr(record, 255, length(record))}" : record]
-  ttl             = local.txt_records[count.index].record.ttl
+resource "aws_route53_record" "records" {
+  for_each        = local.records
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+  name            = each.value.name
+  records         = each.value.records
+  ttl             = each.value.ttl
   allow_overwrite = true
 }
--- packages/infrastructure/aws_dns_records/vars.tf
+++ packages/infrastructure/aws_dns_records/vars.tf
@@ -2,17 +2,17 @@ variable "zones" {
   description = "Zone configurations. Keys are the domain names."
   type = map(object({
     a_records = optional(list(object({
-      subdomain = string
+      subdomain = optional(string, "")
       records   = list(string)
       ttl       = optional(number, 300)
-    })))
+    })), [])
     mx_records = optional(list(object({
-      subdomain = string
+      subdomain = optional(string, "")
       records   = list(string)
       ttl       = optional(number, 86400)
     })), [])
     txt_records = optional(list(object({
-      subdomain = string
+      subdomain = optional(string, "")
       records   = list(string)
       ttl       = optional(number, 300)
     })), [])
@@ -22,5 +22,10 @@ variable "zones" {
       ttl       = optional(number, 300)
     })), [])
   }))
+
+  validation {
+    condition     = alltrue(flatten([for zone, config in var.zones : [for type in keys(config) : [for record in config[type] : !startswith(record.subdomain, ".")]]]))
+    error_message = "Subdomains cannot start with a ."
+  }
 }
 
