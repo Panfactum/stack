929060892760fc8e7a6e8eb317b9d6b6a79896ac
fullykubed <github@fullstackjack.io>
2024-11-13 10:16:53 -0500

    chore: bump aws-ebs-csi-driver to 1.37

--- packages/infrastructure/kube_aws_ebs_csi/vars.tf
+++ packages/infrastructure/kube_aws_ebs_csi/vars.tf
@@ -2,7 +2,7 @@
 variable "aws_ebs_csi_driver_helm_version" {
   description = "The version of the aws-ebs-csi-driver helm chart to deploy"
   type        = string
-  default     = "2.36.0"
+  default     = "2.37.0"
 }
 
 variable "eks_cluster_name" {
