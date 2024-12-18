8cae07a5e01cde6a696c3a517bfa1a42c6a61e0f
James Lee <uptownhr@gmail.com>
2024-12-17 20:29:23 -0800

    fix: replacing all panfactum placeholders

--- packages/website-2/astro.config.mjs
+++ packages/website-2/astro.config.mjs
@@ -3,42 +3,14 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import expressiveCode, { definePlugin } from 'astro-expressive-code'
+import expressiveCode from 'astro-expressive-code'
 import { defineConfig } from 'astro/config'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug'
-import rehypeUrls from 'rehype-urls'
 import rehypeWrap from 'rehype-wrap-all'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
-import {
-  discordServerLink,
-  PANFACTUM_VERSION_PLACEHOLDERS,
-  replaceVersionPlaceholders,
-} from './src/lib/constants'
-
-export function replacePanfactumVariablesPlugin() {
-  return definePlugin({
-    name: 'replace-panfactum-variables',
-    hooks: {
-      preprocessCode: (context) => {
-        context.codeBlock.getLines().forEach((line) => {
-          const foundItems = Object.entries(
-            PANFACTUM_VERSION_PLACEHOLDERS,
-          ).filter(([placeholder, replaceWith]) =>
-            line.text.includes(placeholder),
-          )
-
-          foundItems.forEach(([placeholder, replaceWith]) => {
-            const start = line.text.indexOf(placeholder, replaceWith)
-            line.editText(start, start + placeholder.length, replaceWith)
-          })
-        })
-      },
-    },
-  })
-}
 
 // https://astro.build/config
 export default defineConfig({
@@ -48,7 +20,7 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
-    expressiveCode({ plugins: [replacePanfactumVariablesPlugin()] }),
+    expressiveCode({}),
     mdx(),
     sitemap(),
   ],
@@ -62,30 +34,9 @@ export default defineConfig({
         { selector: 'table', wrapper: 'div.overflow-x-scroll mb-4' },
       ],
       rehypeKatex,
-      [
-        rehypeUrls,
-        {
-          transform: (url) => {
-            if (!url.path) {
-              return
-            }
-            return replaceVersionPlaceholders(url.href).replaceAll(
-              '__discordServerLink__',
-              discordServerLink,
-            )
-          },
-        },
-      ],
     ],
     shikiConfig: {
       wrap: false,
     },
-  },
-
-  redirects: {
-    /*"/docs/framework": {
-      status: 302,
-      destination: "/docs/framework/framework/overview",
-    },*/
   },
 })
---
+++ packages/website-2/src/components/documentation/mdx-components.tsx
@@ -0,0 +1,30 @@
+import {
+  discordServerLink,
+  replaceVersionPlaceholders,
+} from '@/lib/constants.ts'
+
+export const mdxComponents = {
+  code: ({ children, ...props }) => {
+    const child = {
+      ...children,
+      props: {
+        ...children.props,
+        value: replaceVersionPlaceholders(children.props.value),
+      },
+    }
+
+    return <code {...props}>{child}</code>
+  },
+
+  a: ({ children, ...props }) => {
+    const updatedProps = {
+      ...props,
+      href: replaceVersionPlaceholders(props.href).replaceAll(
+        '__discordServerLink__',
+        discordServerLink,
+      ),
+    }
+
+    return <a {...updatedProps}>{children}</a>
+  },
+}
