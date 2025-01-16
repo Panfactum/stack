09c3cdc4efa4b5de5b631d039f8367d4fe2a250c
Dennis Lee <ibanzajoe@gmail.com>
2025-01-16 11:16:36 -0800

    add animation logo to hero (#206)
    1. add first draft of logo animation to hero section of landing page
    <!-- ELLIPSIS_HIDDEN -->
    
    ----
    
    > [!IMPORTANT]
    > Add animated SVG components and CSS animations to the hero section of
    the landing page.
    > 
    >   - **SVG Components**:
    > - Add `CircleSVG`, `RootOrbitSVG`, and `RootOrbitSVG_02` components
    for animated SVG graphics.
    >   - **CSS Animations**:
    > - Add `animation.css` for rotating and moving animations, including
    `rotatingPlanets`, `rotateCube`, and `circlingIcons` keyframes.
    >   - **Page Integration**:
    > - Integrate new SVG components and animations into `index.astro` for
    the hero section of the landing page.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=Panfactum%2Fstack&utm_source=github&utm_medium=referral)<sup>
    for 28dce6d33d7ef1967a40117b8adda68d7b319ad0. It will automatically
    update as commits are pushed.</sup>
    
    <!-- ELLIPSIS_HIDDEN -->

---
+++ packages/website-2/src/components/svg/Circle.tsx
@@ -0,0 +1,3 @@
+export default function CircleSVG() {
+    return <svg fill="none" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><linearGradient id="a" gradientUnits="userSpaceOnUse" x1="54" x2="461" y1="108" y2="401"><stop offset="0" stop-color="#1d3e4f" stop-opacity=".3"/><stop offset="1" stop-color="#9ad3f1"/></linearGradient><circle cx="256" cy="256" r="255" stroke="url(#a)" stroke-width="2"/></svg>
+}
