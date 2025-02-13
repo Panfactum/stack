fa24a67f615c8b10c281831a719352a352f06bfa
James Lee <uptownhr@gmail.com>
2025-02-13 15:36:36 -0800

    updated to use preconditions

--- packages/infrastructure/mongodb_atlas_identity_provider/main.tf
+++ packages/infrastructure/mongodb_atlas_identity_provider/main.tf
@@ -29,21 +29,6 @@ locals {
 
   # Final role mappings (default + only new custom mappings)
   role_mappings = merge(local.default_role_mappings, local.allowed_custom_mappings)
-
-  # List of valid Authentik group names
-  authentik_group_names = [for group in data.authentik_groups.all.groups : group.name]
-
-  # Identify invalid groups (ones that do NOT exist in Authentik)
-  invalid_role_mappings = {
-    for key, roles in local.role_mappings :
-    key => roles if !contains(local.authentik_group_names, key)
-  }
-
-  # Valid role mappings (excluding invalid groups)
-  valid_role_mappings = {
-    for key, roles in local.role_mappings :
-    key => roles if contains(local.authentik_group_names, key)
-  }
 }
 
 
@@ -72,17 +57,8 @@ data "mongodbatlas_federated_settings_identity_provider" "identity_provider_ds" 
 
 data "authentik_groups" "all" {}
 
-# 🚨 Error out if there are invalid group mappings
-resource "null_resource" "invalid_group_check" {
-  count = length(local.invalid_role_mappings) > 0 ? 1 : 0
-
-  provisioner "local-exec" {
-    command = "echo 'ERROR: The following Authentik groups do not exist: ${join(", ", keys(local.invalid_role_mappings))}' && exit 1"
-  }
-}
-
 resource "mongodbatlas_federated_settings_org_role_mapping" "role_mapping" {
-  for_each = local.valid_role_mappings
+  for_each = local.role_mappings
 
   external_group_name    = each.key
   federation_settings_id = var.federation_settings_id
@@ -91,5 +67,13 @@ resource "mongodbatlas_federated_settings_org_role_mapping" "role_mapping" {
   role_assignments {
     org_id = var.organization_id
     roles  = each.value
+  }
+
+  lifecycle {
+    # Precondition to ensure the Authentik group exists
+    precondition {
+      condition     = contains([for group in data.authentik_groups.all.groups : group.name], each.key)
+      error_message = "Authentik group '${each.key}' does not exist."
+    }
   }
 }
