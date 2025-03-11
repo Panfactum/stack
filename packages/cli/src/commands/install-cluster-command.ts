a26433f6362698f17d5c4735a85d7acb183a7332
Seth Alexander <11750061+itzsaga@users.noreply.github.com>
2025-03-11 12:22:29 -0500

    scaffold next steps

---
+++ packages/cli/src/commands/install-cluster-command.ts
@@ -0,0 +1,7 @@
+// top level command for installing a cluster
+
+// must ask about SLA
+// warn that this is not easily changeable
+// must check the region & run script to do that (NOT in global region)
+// must ask for all the variables needed for the install and write them to a temp config file to read from throughout the install
+
