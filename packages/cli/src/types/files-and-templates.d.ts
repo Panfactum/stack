882e7960fdface0684efadc131748cc0a03c8ebf
Seth Alexander <11750061+itzsaga@users.noreply.github.com>
2025-03-18 17:11:07 -0500

    add types for file embeds to not error

---
+++ packages/cli/src/types/files-and-templates.d.ts
@@ -0,0 +1,19 @@
+declare module "*.hcl" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.tf" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.tftpl" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.yaml" {
+  const content: string;
+  export default content;
+}
