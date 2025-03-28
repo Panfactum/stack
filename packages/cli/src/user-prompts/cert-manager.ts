557b455ee14ab04ddc735a347b355164666ac0c0
Seth Alexander <11750061+itzsaga@users.noreply.github.com>
2025-03-28 15:22:07 -0500

    email address validation on input

--- packages/cli/src/user-prompts/cert-manager.ts
+++ packages/cli/src/user-prompts/cert-manager.ts
@@ -9,6 +9,15 @@ export async function certManagerPrompts() {
         "->"
     ),
     required: true,
+    validate: (value) => {
+      // From https://emailregex.com/
+      const emailRegex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!emailRegex.test(value)) {
+        return "Please enter a valid email address";
+      }
+      return true;
+    },
   });
 
   return { alertEmail };
