80dd324a53d5840f014a4409ab413bd6b35641f5
Seth Alexander <11750061+itzsaga@users.noreply.github.com>
2025-04-11 14:35:47 -0500

    fix import paths

--- packages/cli/src/commands/cluster/install/setupInternalClusterNetworking.ts
+++ packages/cli/src/commands/cluster/install/setupInternalClusterNetworking.ts
@@ -1,35 +1,36 @@
+import kubeCiliumTerragruntHcl from "@/templates/kube_cilium_terragrunt.hcl" with { type: "file" };
+import kubeCoreDnsTerragruntHcl from "@/templates/kube_core_dns_terragrunt.hcl" with { type: "file" };
 import { deployModule } from "./deployModule";
-import kubeCiliumTerragruntHcl from "../../../../templates/kube_cilium_terragrunt.hcl" with { type: "file" };
-import kubeCoreDnsTerragruntHcl from "../../../../templates/kube_core_dns_terragrunt.hcl" with { type: "file" };
 import type { InstallClusterStepOptions } from "./common";
 
+export async function setupInternalClusterNetworking(
+  options: InstallClusterStepOptions
+) {
+  const { stepNum } = options;
 
-export async function setupInternalClusterNetworking(options: InstallClusterStepOptions) {
-    const { stepNum } = options;
+  /***************************************************
+   * Deploy the Cilium Module
+   ***************************************************/
+  await deployModule({
+    ...options,
+    stepId: "setupCilium",
+    stepName: "Cilium Setup",
+    moduleDirectory: "kube_cilium",
+    terraguntContents: kubeCiliumTerragruntHcl,
+    stepNum,
+    subStepNum: 1,
+  });
 
-    /***************************************************
-     * Deploy the Cilium Module
-     ***************************************************/
-    await deployModule({
-        ...options,
-        stepId: "setupCilium",
-        stepName: "Cilium Setup",
-        moduleDirectory: "kube_cilium",
-        terraguntContents: kubeCiliumTerragruntHcl,
-        stepNum,
-        subStepNum: 1,
-    })
-    
-    /***************************************************
-     * Deploy the CoreDNS Module
-     ***************************************************/
-    await deployModule({
-        ...options,
-        stepId: "setupCoreDNS",
-        stepName: "CoreDNS Setup",
-        moduleDirectory: "kube_core_dns",
-        terraguntContents: kubeCoreDnsTerragruntHcl,
-        stepNum,
-        subStepNum: 2,
-    })
+  /***************************************************
+   * Deploy the CoreDNS Module
+   ***************************************************/
+  await deployModule({
+    ...options,
+    stepId: "setupCoreDNS",
+    stepName: "CoreDNS Setup",
+    moduleDirectory: "kube_core_dns",
+    terraguntContents: kubeCoreDnsTerragruntHcl,
+    stepNum,
+    subStepNum: 2,
+  });
 }
