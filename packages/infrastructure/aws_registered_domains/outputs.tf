6834da41e553123098fc1305e57a1e404a91001a
fullykubed <github@fullstackjack.io>
2025-04-25 12:45:59 -0400

    fix: aws_registered_domains output

--- packages/infrastructure/aws_registered_domains/outputs.tf
+++ packages/infrastructure/aws_registered_domains/outputs.tf
@@ -1,15 +1,6 @@
 output "domains" {
-  value = { for k in var.domain_names : k => {
-    abuse_contact_email = aws_route53domains_registered_domain.domain[k].abuse_contact_email
-    abuse_contact_phone = aws_route53domains_registered_domain.domain[k].abuse_contact_phone
-    creation_date       = aws_route53domains_registered_domain.domain[k].creation_date
-    expiration_date     = aws_route53domains_registered_domain.domain[k].expiration_date
-    whois_server        = aws_route53domains_registered_domain.domain[k].whois_server
-    registrar_name      = aws_route53domains_registered_domain.domain[k].registrar_name
-    registrar_url       = aws_route53domains_registered_domain.domain[k].registrar_url
-    reseller            = aws_route53domains_registered_domain.domain[k].reseller
-    status_list         = aws_route53domains_registered_domain.domain[k].status_list
-  } }
+  value     = aws_route53domains_registered_domain.domain
+  sensitive = true
 }
 
 output "record_manager_role_arn" {
